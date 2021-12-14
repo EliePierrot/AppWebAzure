fa9c1005b5758ed81bf2ead713ac7728917d30e8
EliePierrot <76158161+EliePierrot@users.noreply.github.com>
2021-12-14 16:25:23 +0100

    Update tableau.php

--- tableau.php
+++ tableau.php
@@ -3,10 +3,10 @@
   <head>
     <meta charset="utf-8">
     <title>Tableau des tickets</title>
-    <link rel="icon" type="image/png" href="images/logo.png" />
+    <link rel="icon" type="image/png" href="images/logo.png" /><link rel="stylesheet" href="monstyle.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 
-<link rel="stylesheet" href="monstyle.css">
+
   </head>
 
 
